87377b9d9a5e23de9c636b37549ad0c87d3bf8af
JorgeRondon93 <jorge.rondon@simetrik.com>
2023-04-14 17:19:17 -0500

    Modificación de documentación

--- main.py
+++ main.py
@@ -1,42 +1,69 @@
 """
-Spanish: Modulo para manipulación de los elementos de una lista
+Español: Modulo para manipulación de los elementos de una lista
 
-Lleva a cabo la modificación de posiciones de una lista
-Este modulo esta constituido por una clase y una función que permiten invertir la posición de los elementos de una lista
+    Lleva a cabo la modificación de posiciones de una lista
+    Este modulo esta constituido por una clase y una función que permiten invertir la posición de los elementos de una lista
 
-Funciones Disponible:
-- list_inverter: Invierte la posición de los elementos de una lista que es entregada como su argumento.
+    Funciones Disponible:
+    - list_inverter: Invierte la posición de los elementos de una lista que es entregada como su argumento.
 
-Clases Disponibles:
-- ListHandler: Clase para realizar al manipulación de la posición de los elementos de una lista
+    Clases Disponibles:
+    - ListHandler: Clase para realizar al manipulación de la posición de los elementos de una lista
 
-------------
+English: Module to manipulate list elements.
 
-English:
+    Modify the element positions of a list.
+    This module consists of a class and a function that allow to invert the position of a list elements.
+
+    Available Functions:
+    - list_inverter: Inverts the position of the elements of a list that is given as its argument.
+
+    Available Classes:
+    - ListHandler: Class to manipulate the position of the list elements.
 
 """
 
 class ListHandler:
-    """ Clase para realizar la manipulación de la posición de los elementos de una lista
+    """ Español: Clase para realizar la manipulación de la posición de los elementos de una lista
 
-    Esta clase contiene un metodo que permite realizar la inversión de la posición de los elementos
-    de una lista que se le entrega como argumento.
+            Esta clase contiene un metodo que permite realizar la inversión de la posición de los elementos
+            de una lista que se le entrega como argumento.
+
+        English: Class to manipulate the position of the elements of a list.
+
+            This class contains a method that allows to invert the position of the elements of a list given as an argument.
     """
+    #def __init__(self):
+        #self.or_list = None
 
     def list_inverter(self, original_list:list) -> list:
-        """ Invierte las posiciones de los elementos de la lista
+        """ Español: Invierte las posiciones de los elementos de la lista
 
-        Esta función toma la lista que es entregada como argumento e invierte la posición de sus elementos.
-        
-        Args:
-            - original_list (list): Lista que se desea invertir
+                Esta función toma la lista que es entregada como argumento e invierte la posición de sus elementos.
+                
+                Args:
+                    - original_list (list): Lista que se desea invertir
 
-        Returns:
-            - list: Lista invertida
+                Retorno:
+                    - list: Lista invertida
 
-        Constrains:
-            - La función no permite procesar listas vacias.
-            - La función no procesa tipos de datos diferentes a listas
+                Restricciones:
+                    - La función no permite procesar listas vacias.
+                    - La función no procesa tipos de datos diferentes a listas
+
+            English: Inverts the position of the elements of a list.
+
+                This function takes the list that is given as an argument and inverts the position of its elements.
+                
+                Args:
+                    - original_list (list): Lista to invert
+
+                Returns:
+                    - invereted_list (list): inverted list
+
+                Constrains:
+                    - Function does not allow empty lists.
+                    - Function just process list datatypes.
         """
 
         if type(original_list)!=list:
@@ -47,10 +74,11 @@ class ListHandler:
         
         else:
             inverted_list =  original_list[::-1]
+            self.or_list = inverted_list
             return inverted_list
 
 
-
+#Script para ejecutar este modulo desde consola:
 #if __name__ == "__main__":
 
  #   separator = str(input("ingrese el separador que desea usar en la lista: ")) or ","
@@ -59,5 +87,6 @@ class ListHandler:
  #   new_obj = ListHandler()
  #   inverted_list = new_obj.list_inverter(original_list=user_list)
 
- #  print(f"Original List: {user_list}")
+ #   print(f"Original List: {user_list}")
  #   print(f"Inverted List: {inverted_list}")
+ #   print(f"or_list atribute: {new_obj.or_list}")
--- tests/test_inverter.py
+++ tests/test_inverter.py
@@ -1,11 +1,20 @@
-""" Modulo para realizar pruebas unitarias de la función list_inverter de la clase ListHandler
+""" Español: Modulo para realizar pruebas unitarias de la función list_inverter de la clase ListHandler
 
-    Lleva a cabo un conjunto de pruebas unitarias haciendo uso del framework pytest
-    
-    Funciones Disponibles:
-        - test_inverter: Prueba que la inversión de una lista se haga de forma correcta
-        - test_invalid_datatype: Prueba que la función no procesa tipos de datos diferentes a listas.
-        - test_void_list: Prueba que la función no procesa listas vacias
+        Lleva a cabo un conjunto de pruebas unitarias haciendo uso del framework pytest
+        
+        Funciones Disponibles:
+            - test_inverter: Prueba que la inversión de una lista se haga de forma correcta
+            - test_invalid_datatype: Prueba que la función no procesa tipos de datos diferentes a listas.
+            - test_void_list: Prueba que la función no procesa listas vacias
+
+    English: Module to perform unit tests of the list_inverter function of the ListHandler class.
+
+        Performs a set of unit tests using the pytest framework.
+        
+        Available functions:
+            - test_inverter: Tests that the inversion of a list is done correctly.
+            - test_invalid_datatatype: Tests that the function does not process data types other than lists.
+            - test_void_list: Tests that the function does not process empty lists.
 """
 
 
@@ -20,11 +29,17 @@ from Pruebas_Unitarias.main import ListHandler
         ]
 )
 def test_inverter(lista: list, expected: list) -> None:
-    """ Prueba que la inversión de una lista se haga de forma correcta y que procesa datos de tipo lista.
+    """ Español: Prueba que la inversión de una lista se haga de forma correcta y que procesa datos de tipo lista.
 
         Args:
             lista (list): lista a invertir
-            expected (list): Lista invertida que se espera recibir.
+            expected (list): Lista invertida que se espera recibir como retorno.
+
+        English: Tests that the inversion of a list is done correctly and that it processes data of type list.
+
+        Args:
+            list (list): list to invert.
+            expected (list): inverted list expected to receive as return.
     
     """
     my_obj = ListHandler()
@@ -41,10 +56,15 @@ def test_inverter(lista: list, expected: list) -> None:
     ]
 )
 def test_invalid_datatype(lista:any)->None:
-    """ Prueba que la función no procesa tipos de datos diferentes a listas.
+    """ Español: Prueba que la función no procesa tipos de datos diferentes a listas.
 
         Args:
             lista (any): Cualquier tipo de dato.
+
+        English: Tests that the function does not process data types other than lists.
+
+        Args:
+            list (any): any data type.
     
     """
     my_obj = ListHandler()
@@ -59,10 +79,15 @@ def test_invalid_datatype(lista:any)->None:
         ]
 )
 def test_void_list(lista:list)-> None:
-    """ Prueba que la función no procesa listas vacias.
+    """ Español: Prueba que la función no procesa listas vacias.
 
         Args:
             lista (list): Lista vacia
+
+        English: Tests that the function does not process empty lists.
+
+        Args:
+            list (list): Empty list
     
     """
     my_obj = ListHandler()
